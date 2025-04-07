c1906ce47cee768cdbaf25210ea7b7617bb0b1ca
Shrawan <shrawanmainali261@gmail.com>
2025-04-07 22:06:04 +0545

    homepage header commit

---
+++ includes/header.php
@@ -0,0 +1,72 @@
+<html>
+<header>
+  <div class="default-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-md-2">
+          <div class="logo"> <a href="index.html"><img src="assets/images/logo.png" alt="image"/></a> </div>
+        </div>
+        <div class="col-sm-9 col-md-10">
+          <div class="header_info">
+            <div class="header_widgets">
+              <div class="circle_icon"> <i class="fa fa-envelope" aria-hidden="true"></i> </div>
+              <p class="uppercase_text">For Support Mail: </p>
+              <a href="mailto:support@example.com">support@example.com</a> </div>
+            <div class="header_widgets">
+              <div class="circle_icon"> <i class="fa fa-phone" aria-hidden="true"></i> </div>
+              <p class="uppercase_text"> Helpline</p>
+              <a href="tel:+1234567890">+1234567890</a> </div>
+            <div class="social-follow">
+
+            </div>
+            <div class="login_btn"> <a href="#loginform" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <nav id="navigation_bar" class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <button id="menu_slide" data-target="#navigation" aria-expanded="false" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      </div>
+      <div class="header_wrap">
+        <div class="user_login">
+          <ul>
+            <li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle" aria-hidden="true"></i> Guest <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+              <ul class="dropdown-menu">
+                <li><a href="profile.html">Profile Settings</a></li>
+                <li><a href="update-password.html">Update Password</a></li>
+                <li><a href="my-booking.html">My Booking</a></li>
+                <li><a href="post-testimonial.html">Post a Testimonial</a></li>
+                <li><a href="my-testimonials.html">My Testimonial</a></li>
+                <li><a href="logout.html">Sign Out</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div class="header_search">
+          <div id="search_toggle"><i class="fa fa-search" aria-hidden="true"></i></div>
+          <form action="search.html" method="post" id="header-search-form">
+            <input type="text" placeholder="Search..." name="searchdata" class="form-control" required="true">
+            <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+          </form>
+        </div>
+      </div>
+      <div class="collapse navbar-collapse" id="navigation">
+        <ul class="nav navbar-nav">
+          <li><a href="index.html">Home</a> </li>
+
+          <li><a href="page.html?type=aboutus">About Us</a></li>
+          <li><a href="car-listing.html">Car Listing</a>
+          <li><a href="page.html?type=faqs">FAQs</a></li>
+          <li><a href="contact-us.html">Contact Us</a></li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+</header>
+</html>
